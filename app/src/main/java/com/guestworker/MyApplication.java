5d3a46f8398b0b7a13dacc5c9f817e03c6707d1b
moxiaoting <moxiaoting163@163.com>
2019-04-28 13:15:19 +0800

    bugly的接入

--- app/src/main/java/com/guestworker/MyApplication.java
+++ app/src/main/java/com/guestworker/MyApplication.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
-import android.util.Log;
+import android.text.TextUtils;
 
 import com.blankj.utilcode.util.Utils;
 import com.guestworker.netwrok.ApplicationComponent;
 import com.guestworker.netwrok.DaggerApplicationComponent;
 import com.guestworker.util.DebugHelper;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -41,6 +45,8 @@ public class MyApplication extends Application {
         UMConfigure.init(this, "5cc030654ca357ce9a0001b9","umeng",UMConfigure.DEVICE_TYPE_PHONE, "");
         //微信
         PlatformConfig.setWeixin("wxfd80aece07456d53", "68f2c6ee2bab0f6e98f2f9ec547b2292");
+
+        initBugly();
     }
 
     @Override
@@ -71,5 +77,54 @@ public class MyApplication extends Application {
             }
         }
         activityList.clear();
+    }
+
+    /**
+     * 初始化bugly
+     */
+    private void initBugly(){
+        Context context = getApplicationContext();
+        // 获取当前包名
+        String packageName = context.getPackageName();
+        // 获取当前进程名
+        String processName = getProcessName(android.os.Process.myPid());
+        // 设置是否为上报进程
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setBuglyLogUpload(processName == null || processName.equals(packageName));
+        if(DebugHelper.getDebug()){
+            strategy.setAppVersion("开发版本");
+        }
+        // 初始化Bugly
+        CrashReport.initCrashReport(getApplicationContext(), "9f3a3a8257", false,strategy);
+    }
+
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
     }
 }
